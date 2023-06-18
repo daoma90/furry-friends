ec2b92404af2636666a66ac10a95ef20ad55459b
David <Daoma90@gmail.com>
2023-06-18 13:27:41 +0200

    Add sorting functions

---
+++ src/utils/sorting.ts
@@ -0,0 +1,31 @@
+import { Cat } from "../types/cat";
+
+export const sortByName = (array: Array<Cat>) => {
+  const copy = [...array];
+  const sorted = copy.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name < b.name) {
+      return 1;
+    }
+    return 0;
+  });
+  console.log("sorted", sorted);
+  return sorted;
+};
+
+export const sortByGender = (array: Array<Cat>) => {
+  const copy = [...array];
+  const sorted = copy.sort((a, b) => {
+    if (a.gender < b.gender) {
+      return -1;
+    }
+    if (a.gender < b.gender) {
+      return 1;
+    }
+    return 0;
+  });
+  console.log("sorted", sorted);
+  return sorted;
+};
